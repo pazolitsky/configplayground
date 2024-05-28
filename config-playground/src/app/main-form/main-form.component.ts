33f607474796ee14f1e5031bc48c191721d72fb2
Nour Khalid Hallack <halaq1999.nour@gmail.com>
2024-05-28 11:39:37 +0300

    Second Commit Nour + Yael

--- config-playground/src/app/main-form/main-form.component.ts
+++ config-playground/src/app/main-form/main-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-main-form',
@@ -12,4 +14,44 @@ export class MainFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  form = new FormGroup({});
+  model = { input1: '', input2: '', input3: '' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'domain',
+      type: 'input',
+      templateOptions: {
+        label: 'Domain',
+        placeholder: 'Enter Domain',
+        required: true
+      }
+    },
+    {
+      key: 'institution',
+      type: 'input',
+      templateOptions: {
+        label: 'Institution',
+        placeholder: 'Enter Institution value',
+        required: true
+      }
+    },
+    {
+      key: 'view',
+      type: 'input',
+      templateOptions: {
+        label: 'View',
+        placeholder: 'Enter view value',
+        required: true
+      }
+    }
+  ];
+
+  onSubmit() {
+    if (this.form.valid) {
+      confirm('Configuration submitted!');
+    }
+  }
+
 }
+
+
