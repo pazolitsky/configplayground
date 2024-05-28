49b33a13b24a9bd69a6b4bd05301294b7b796ffb
NourHallack <halaq1999.nour@gmail.com>
2024-05-28 11:23:05 +0300

    create form comp

--- config-playground/src/app/app.module.ts
+++ config-playground/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainFormComponent } from './main-form/main-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
